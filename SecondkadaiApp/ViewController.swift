f6d0d47987b8531882a6a51f8f1a6a02398d1b57
koyanagibiz <koyanagibiz@gmail.com>
2018-10-17 14:44:32 +0900

    Initial Commit

---
+++ SecondkadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondkadaiApp
+//
+//  Created by 小柳貴志 on 2018/10/17.
+//  Copyright © 2018年 takashi.koyanagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
